f07340df30e2eb6ff923bdf63e8626d080283926
Reliable7650 <119016160+Reliable7650@users.noreply.github.com>
2022-12-18 16:41:45 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app!,我尝试在这里学习并开始" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
